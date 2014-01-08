fa1db24002ecdf83c357f964a7ff0f91d4b03f53
cristianomatos <cristianobmatos@gmail.com>
2014-01-08 07:19:43 -0200

    Fix in navbar
    Change-Id: I34f452e0cd2c29e3dc24202f54968eb6477dcddd

--- src/com/android/settings/paranoid/NavBar.java
+++ src/com/android/settings/paranoid/NavBar.java
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 
-package com.android.settings.paranoid.NavBar;
+package com.android.settings.paranoid;
 
 import android.app.Activity;
 import android.app.AlertDialog;
